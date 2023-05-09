499c0cec40ea4c366f16ed86a3a93c49f117bd6b
Cyril BARTZ <bartzcyril@gmail.com>
2023-05-09 12:43:47 +0200

    amélioration de l'accessibilité

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 
     const footer = (
         <footer>
-            <div className="border-t border-slate-400 mt-12 py-6 text-center text-slate-400">
+            <div className="border-t border-slate-400 mt-12 py-6 text-center">
                 <h3>Cyril</h3>
             </div>
         </footer>
