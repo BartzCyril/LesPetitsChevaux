3e01d6380d4e75a977773af6c85302e7789b22bf
Cyril BARTZ <bartzcyril@gmail.com>
2023-05-14 20:46:28 +0200

    correction erreur ts

--- src/components/Dice.tsx
+++ src/components/Dice.tsx
@@ -10,25 +10,27 @@ export function Dice({onDiceRoll}: DiceProps) {
     const buttonRef = useRef(null)
     const handleDiceClick = () => {
         const dice = buttonRef.current
-        const dots = dice.querySelectorAll('.dot')
-        // Ajout de l'animation de lancer de dé
-        dice.classList.add('roll-animation')
-        // Réinitialisation des points
-        dots.forEach(dot => {
-            dot.style.display = 'none';
-        });
-       setTimeout(() => {
-            // Génération d'un nombre aléatoire entre 1 et 6
-            //const randomNumber = Math.floor(Math.random() * 6) + 1;
-            const randomNumber = 6
-            onDiceRoll(randomNumber)
-            // Affichage du nombre de points correspondant
-            for (let i = 0; i < randomNumber; i++) {
-                dots[i].style.display = 'block';
-            }
-            // Suppression de l'animation et réactivation du bouton
-            dice.classList.remove('roll-animation');
-        }, 1000);
+        if (dice) {
+            const dots = dice.querySelectorAll('.dot')
+            // Ajout de l'animation de lancer de dé
+            dice.classList.add('roll-animation')
+            // Réinitialisation des points
+            dots.forEach(dot => {
+                dot.style.display = 'none';
+            });
+            setTimeout(() => {
+                // Génération d'un nombre aléatoire entre 1 et 6
+                //const randomNumber = Math.floor(Math.random() * 6) + 1;
+                const randomNumber = 6
+                onDiceRoll(randomNumber)
+                // Affichage du nombre de points correspondant
+                for (let i = 0; i < randomNumber; i++) {
+                    dots[i].style.display = 'block';
+                }
+                // Suppression de l'animation et réactivation du bouton
+                dice.classList.remove('roll-animation');
+            }, 1000);
+        }
     }
     return (
         <button className="dice" ref={buttonRef} onClick={handleDiceClick}>
