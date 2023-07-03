3511ca0b03ce63889c53c8b64627c84db23b44e3
Cyril BARTZ <bartzcyril@gmail.com>
2023-07-03 22:24:29 +0200

    amélioration style

--- src/components/ProgressGame.tsx
+++ src/components/ProgressGame.tsx
@@ -2,7 +2,7 @@ export function ProgressGame() {
     return (
         <section className="bg-white dark:bg-gray-900">
             <div className="py-16 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
-                <h2 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Comment se déroule un début de partie ?</h2>
+                <h2 className="mb-8 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Comment se déroule un début de partie ?</h2>
                 <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">
                     Pour commencer, vous devez choisir votre couleur !</p>
                 <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">
