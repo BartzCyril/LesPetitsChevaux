e5d74d7f4f7065f7232928194890559f6170b41d
Cyril BARTZ <bartzcyril@gmail.com>
2023-07-06 18:40:51 +0200

    correction style

--- src/components/ProgressGame.tsx
+++ src/components/ProgressGame.tsx
@@ -1,15 +1,15 @@
 export function ProgressGame() {
     return (
-        <section className="bg-white dark:bg-gray-900">
+        <section className="bg-white">
             <div className="py-16 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
-                <h2 className="mb-8 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Comment se déroule un début de partie ?</h2>
-                <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">
+                <h2 className="mb-8 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl">Comment se déroule un début de partie ?</h2>
+                <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48">
                     Pour commencer, vous devez choisir votre couleur !</p>
-                <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">
+                <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48">
                     Ensuite, vous devez cliquer sur la roue pour déterminer le joueur qui commence la partie.</p>
-                <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">
+                <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-480">
                     Après, chaque joueur lance le dé trois fois de suite jusqu&apos;à ce qu&apos;un joueur obtienne un six.</p>
-                <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">
+                <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48">
                     Bravo ! La partie est lancée ! Bonne chance :)</p>
             </div>
         </section>
